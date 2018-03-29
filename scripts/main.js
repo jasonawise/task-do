d6ac5c8cff433734d2e82df78a6ed3abccb5a3b8
Jason Wise <jasonandrewwise@gmail.com>
2018-03-28 20:03:58 -0400

    creating remove task function

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
  /* This functions adds a task to the to-do-list section and clears out the placeholder */
  const addTask = function()
  {
-     // Get the input of the task
-     let task = document.getElementById('new-task');
-     
-     // Set the const for the to do list
-     const toDoList = document.getElementById('to-do-list');
+    // Get the input of the task
+    let task = document.getElementById('new-task');
+    
+    // Set the const for the to do list
+    const toDoList = document.getElementById('to-do-list');
 
-     // Create the new to do item element
-     let toDoItem = document.createElement('li');
+    // Create the new to do item element
+    let toDoItem = document.createElement('li');
+    toDoItem.id = "task" + taskNumber;
 
-     // Sets the new to do item
-     toDoItem.innerHTML = task.value;
+    // Sets the new to do item
+    toDoItem.innerHTML = task.value + "<span><button id=\"delete-task\" onclick=\"deleteTask()\">X</button></span>";
      
     if (isBlank(task.value))
     {
@@ -25,6 +26,11 @@
         toDoList.appendChild(toDoItem);
     }
 
-     // Clear our input placeholder
-     clearPlaceholder(task);
+    // Clear our input placeholder
+    clearPlaceholder(task);
+ }
+
+ const deleteTask = function()
+ {
+    
  }
