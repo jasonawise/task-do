273a1b780d5a22dad09763f1383b49d5a5ac32c8
Jason Wise <jasonandrewwise@gmail.com>
2018-04-09 21:50:19 -0400

    adding vue.js file to hold the vue js

---
+++ scripts/vue.js
@@ -0,0 +1,10 @@
+var toDoList = new Vue({
+    el: '#to-do-list',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  })
